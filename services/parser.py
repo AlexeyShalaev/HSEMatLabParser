58351d816e215f642cbfc406d565263b1a6d7a5c
qwatro2 <qwatro2>
2023-03-13 19:53:25 +0300

    upd parser

--- services/parser.py
+++ services/parser.py
@@ -4,18 +4,25 @@ import requests
 from bs4 import BeautifulSoup as bs
 
 
-def get_data(link, year):
+def get_data(link, year) -> list[tuple[str, str]]:
     r = requests.get(f'{link}/result{year}-{year + 1}.php')
     if r.ok:
         soup = bs(r.text, "html.parser")
         table = soup.find('table')
         hrefs = table.find_all('a', href=True)
+        res = []
         for href in hrefs:
             url = f"{link}/{href['href'][1:]}"
             resp = requests.get(url)
             if resp.ok:
-                print(resp.content)
-                return None
+                resp.encoding = 'utf8'
+                res.append((url, resp.text))
+
+                # todo: убрать потом
+                if len(res) == 10:
+                    return res
+
+        return res
 
 
 def parse_matlab() -> dict:
@@ -26,9 +33,11 @@ def parse_matlab() -> dict:
     courses = ['matlaba', 'matlabma']
 
     res = dict()
-    # todo: ну ты понял в res все типо {'https://serjmak.com/2students/matlaba/test2/2021-2022/Abulhanov.txt': 'content', ...}
     for course in courses:
-        for year in range(start_year, now.year):
-            get_data(f'{address}/{course}', year)
+        for year in range(start_year, now.year - 1):
+            link = f'{address}/{course}'
+            data = get_data(link, year)
+            for url, content in data:
+                res[url] = content
 
     return res
